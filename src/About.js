3b1347c6412fb6b13e69fd2f0a27cbca3ba1e9e3
fran <fran22345@gmail.com>
2023-11-20 17:23:52 -0300

    Agregados archivos desde el repositorio del tercero

--- src/About.js
+++ src/About.js
@@ -55,7 +55,7 @@ export default function About() {
         <Grid direction="row" container justify="center" alignItems="center" className={classes.cont}>
             <Grid item xs={12} lg={6}>
                 <Typography variant="h2" gutterBottom component="p">
-                    About me
+                    Sobre mi:
                 </Typography>
                 <Typography variant="h5" gutterBottom component="p">
                     {about.description}
